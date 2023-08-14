f11a70cd65b950a6170a9f06924c7dc82aa4fb2b
Aaron Jackson <aaron.jcksn@outlook.com>
2023-08-14 16:13:51 -0500

    Added rough working code.

--- js/app.js
+++ js/app.js
@@ -15,3 +15,51 @@ window.onload = () => {
     items.addEventListener("click", removeItem);
 };
 
+
+function addItem(e) {
+    e.preventDefault();
+
+    if (submit.value != "Submit") {
+        console.log("Hello");
+
+        editItem.target.parentNode.childNodes[0].data = document.getElementById("item").value;
+
+        submit.value = "Submit";
+        document.getElementById("item").value = "";
+
+        document.getElementById("lblsuccess").innerHTML = "Text edited successfully";
+        document.getElementById("lblsucess").style.display = "block";
+        
+        setTimeout(function() {
+            document.getElementById("lblsuccess").style.display = "none";
+        }, 2000);
+        
+        return false;
+    }
+
+    let newItem = document.getElementById("item").value;
+    if (newItem.trim() == "" || newItem.trim() == null) 
+        return false;
+    else
+        document.getElementById("item").value = ""; 
+
+    let li = document.createElement("li");
+    li.className = "list-group-item";
+
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "btn-danger btn btn-sm float-right delete";
+    deleteButton.appendChild(document.createTextNode("X"));
+
+    let editButton = document.createElement("button");
+    editButton.className = "btn-success btn btn-sm float-right edit";
+    editButton.appendChild(document.createTextNode("Edit"));
+
+
+    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(deleteButton);
+    li.appendChild(editButton);
+
+    items.appendChild(li);
+
+    }
+
